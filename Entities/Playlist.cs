d6bd931ec2e6e41bd9380d58626574b66bf73ca5
busee <busee@BUSE_EER>
2023-09-24 16:36:29 +0300

    Add project files.

---
+++ Entities/Playlist.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spotify_deneme.Entities
+{
+    internal class Playlist
+    {
+        public Guid Id { get; set; }
+        private List<Song> Songs { get; set; }
+
+        private Random random;
+
+
+        public Playlist(Song firstSong)
+        {
+            Id = Guid.NewGuid();
+            Songs = new List<Song>();
+            random = new Random();
+            AddSong(firstSong);
+        }
+
+        public void AddSong(Song song)
+        {
+            if (song != null)
+                Songs.Add(song);
+        }
+
+        public string GetSongs()
+        {
+            
+            return string.Join("\n", Songs.Select(x => $"{x.Title} - {x.Composer}" ));
+        }
+
+        public void ShuffleSongs()
+        {
+            int count = Songs.Count;
+            
+            while (count > 0)
+            {
+                count--;
+
+                Song song = Songs[count];  
+                int randomIndex = random.Next(count);
+                Songs[count] = Songs[randomIndex];
+                Songs[randomIndex] = song;
+            }
+        }
+    }
+}
+
+
+      
+
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using spotify_deneme.Entities;
+using spotify_deneme.Services;
+
+Console.WriteLine("Spotify - Music for Noone");
+
+var song1=new Song("Requiem", "Mozart", "Orcehtre de France", null);   
+var song2=new Song("Sonata Major", "Beethoven", "Fazıl Say", "Warner Music");
+var song3 = new Song("Moonlight", "Beethoven", "Various Artists", "Classical Records");
+var song4 = new Song("Four Seasons", "Vivaldi", "Chamber", "Baroque");
+var song5 = new Song("Symphony no9", "Beethoven", "Philharmonic", "Classic Harmony");
+
+var playlist1 = new Playlist(song1); 
+playlist1.AddSong(song2);
+playlist1.AddSong(song3);
+playlist1.AddSong(song4);
+playlist1.AddSong(song5);
+
+Console.WriteLine("Before Shuffle");
+Console.WriteLine(playlist1.GetSongs());
+
+playlist1.ShuffleSongs();
+
+Console.WriteLine("\n\nAfter Shuffle");
+Console.WriteLine(playlist1.GetSongs());
+
+NotepadService notepadService = new ();
+notepadService.WriteToNotepad(playlist1);
